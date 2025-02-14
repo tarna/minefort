77055d4a088b63a02d507b18925aa9a0a9ccf561
tarna <46247728+tarna@users.noreply.github.com>
2025-02-14 16:57:05 -0500

    docs: add jsdocs to methods

--- src/blog/BlogManager.ts
+++ src/blog/BlogManager.ts
@@ -2,24 +2,41 @@ import { Minefort } from '../Minefort';
 import { Article } from './Article';
 import { BlogListResponse, BlogResponse } from './BlogResponse';
 
+/**
+ * The Minefort Blog Manager
+ */
 export class BlogManager {
     client: Minefort;
     constructor(client: Minefort) {
         this.client = client;
     }
 
+    /**
+     * Get an article by its slug
+     * @param slug the slug of the article
+     * @returns {Promise<Article>}
+     * @example const article = await minefort.blog.getArticle('how-to-make-an-anvil-in-minecraft);
+     */
     async getArticle(slug: string) {
         return await this.client.request<BlogResponse>(`/blog/articles/${slug}`, {
             method: 'GET',
         }).then(res => new Article(this.client, res.result));
     }
 
+    /**
+     * Get a list of articles
+     * @returns {Promise<Article[]>}
+     */
     async getArticles() {
         return await this.client.request<BlogListResponse>('/blog/articles', {
             method: 'GET',
         }).then(res => res.result.map(article => new Article(this.client, article)));
     }
 
+    /**
+     * Get a list of featured articles
+     * @returns {Promise<Article[]>}
+     */
     async getFeaturedArticles() {
         return await this.client.request<BlogListResponse>('/blog/featured', {
             method: 'GET',
--- src/server/ServerManager.ts
+++ src/server/ServerManager.ts
@@ -2,12 +2,23 @@ import { Minefort } from '../Minefort';
 import { Server } from './Server';
 import { ServerListResponse } from './ServerResponse';
 
+/**
+ * The Minefort Server Manager
+ */
 export class ServerManager {
     client: Minefort;
     constructor(client: Minefort) {
         this.client = client;
     }
 
+    /**
+     * Get a list of online servers
+     * @param paginationSkip the number of servers to skip
+     * @param limit the number of servers to return
+     * @param sortField the field to sort by
+     * @param sortOrder the order to sort by 
+     * @returns {Promise<Server[]>}
+     */
     async getOnlineServers({
         paginationSkip,
         limit,
